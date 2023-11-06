902ef87b7e53383a5d839dfcb57b97e8d9a560e6
f.salamov <farhaad1987@gmail.com>
2023-11-06 22:33:50 +0400

    Code leetcode commit

---
+++ src/main/java/org/example/ValidPalindromeApp.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class ValidPalindromeApp {
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String text = "A man, a plan, a canal: Panama";
+
+        boolean result = isPalindrome(text);
+        System.out.println(result);
+    }
+}
