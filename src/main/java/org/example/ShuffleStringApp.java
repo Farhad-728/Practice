7b5767628452bf1fd81668dbccc924e305b588dd
f.salamov <farhaad1987@gmail.com>
2023-11-11 13:44:06 +0400

    Code leetcode commit

---
+++ src/main/java/org/example/ShuffleStringApp.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ShuffleStringApp {
+    public static String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+
+        String newResult = restoreString(s, indices);
+
+        System.out.println(newResult);
+    }
+}
