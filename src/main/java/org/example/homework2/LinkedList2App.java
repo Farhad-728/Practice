1505905131aec31ee424efe8d3b6cf3eb08db7a0
f.salamov <farhaad1987@gmail.com>
2023-11-16 21:44:11 +0400

    Code leetcode commit

---
+++ src/main/java/org/example/homework2/LinkedList2App.java
@@ -0,0 +1,23 @@
+package org.example.homework2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedList2App {
+    public static void main(String[] args) {
+        LinkedList<Integer> nums = new LinkedList<>();
+        nums.add(33);
+        nums.add(22);
+        nums.add(5);
+        nums.add(18);
+        nums.add(1);
+
+        System.out.println("Original numbers: " + nums);
+
+        List<Integer> subList = nums.subList(1, 2);
+
+//        nums.subList(1, 3);
+
+        System.out.println("SubList numbers: " + subList);
+    }
+}
