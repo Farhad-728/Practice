4f14d6df7fc728336155404acdee6d9795b75d74
f.salamov <farhaad1987@gmail.com>
2024-03-16 22:05:32 +0400

    divisible and non-divisible sums Difference task leetcode

---
+++ src/main/java/org/example/homework5/SumsDifference.java
@@ -0,0 +1,22 @@
+package org.example.homework5;
+
+public class SumsDifference {
+    public static int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m != 0) {
+                num1 += i;
+            } else {
+                num2 += i;
+            }
+        }
+        return num1 - num2;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 3;
+        differenceOfSums(10, 3);
+    }
+}
