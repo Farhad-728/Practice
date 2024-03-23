3976ccc74d3023a3701916d5b97343c370feae29
f.salamov <farhaad1987@gmail.com>
2024-03-23 19:50:55 +0400

    reverse words in a string task leetcode

---
+++ src/main/java/org/example/homework6/ReverseWordsString.java
@@ -0,0 +1,40 @@
+package org.example.homework6;
+
+import java.util.Arrays;
+
+public class ReverseWordsString {
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder reversedSentence = new StringBuilder();
+        for (String word : words) {
+            String reversedWord = reverseWord(word);
+            reversedSentence.append(reversedWord).append(" ");
+        }
+        return reversedSentence.toString().trim();
+    }
+
+    private static String reverseWord(String word) {
+        char[] charArray = word.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            // Move pointers inward
+            left++;
+            right--;
+        }
+
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        String newWord = reverseWords(s);
+        System.out.println(newWord);
+    }
+}
